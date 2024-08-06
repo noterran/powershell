ef4e14dce2ac58232e06b7fe88340be3e5af94df
Ole_Anders Herland <ole.anders.herland@marcello.no>
2024-08-06 15:02:27 +0200

    Fixed silent install pswinupdate

--- Get-wuinstall force all.ps1
+++ Get-wuinstall force all.ps1
@@ -1,7 +1,7 @@
     <# .SYNOPSIS
          
     .DESCRIPTION
-         This routine is ment to force install all available updates including drivers and BIOS and force restart the computer
+         This routine is ment to install PSWindowsUpdate then force install all available updates including drivers and BIOS.
 
     .NOTES
          Author     : Ole Anders Herland
@@ -9,12 +9,30 @@
          Revised    : 05.08.2024
          Version    : 1.00
 
-         Comments   : This routine requires PSWindowsUpdate module
+         Comments   :
     .LINK
         
     #>
 
     Try {
+        if(-not (Get-Module PSWindowsUpdate -ListAvailable)){
+            Install-Module PSWindowsUpdate -Confirm:$False -Force
+        }
+        else{Write-Host "Updating PSWindowsUpdate"
+        Update-Module PSWindowsUpdate -Confirm:$False -Force
+        }
+    }
+    Catch {
+        $ErrorArray = $_.Exception
+        $ErrorRecord = $ErrorArray.ErrorRecord | Out-String
+        #If the component fails write to the ouput and give a fail exit code
+        Write-Host "Routine for installation of PSWindowsUpdate"
+        Write-Host "$ErrorRecord"
+    
+        Exit 1
+    }
+
+     Try {
         Write-Host "Installing all available updates"
         Get-wuinstall -microsoftupdate -acceptall -autoreboot:$false -install
         
@@ -31,7 +49,7 @@
     
     #On success write to the output and exit with a success code
     Write-Host "Routine for installation of updates completed successfully"
-    Write-Host "Restarting the computer"
-    Restart-computer -force
+    #Write-Host "Restarting the computer"
+    #Restart-computer -force
     
     Exit 0
