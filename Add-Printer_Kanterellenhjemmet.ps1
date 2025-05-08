cf672958f7446807fd5ce07876450e924de00b35
Ole_Anders Herland <ole.anders.herland@marcello.no>
2025-05-08 10:52:54 +0200

    Changed to download drivers

--- Add-Printer_Kanterellenhjemmet.ps1
+++ Add-Printer_Kanterellenhjemmet.ps1
@@ -1,1 +1,34 @@
-Add-PrinterPort -Name "HP Color LaserJet Pro MFP" -PrinterHostAddress "192.168.141.251"
+# Variables
+$driverDownloadPath = "https://ftp.hp.com/pub/softlib/software13/printers/UPD/upd-pcl6-x64-7.6.0.26178.zip"
+$portName = "IP_192.168.141.251" # Printer port name
+$portAddress = "192.168.141.251" # Printer IP
+$printerName = "HP Color LaserJet Pro MFP M479fdw" # Printer display name
+$driverName = "HP Universal Printing PCL 6" # Must be exact! Install driver on your computer and copy its name and paste here!
+# Do not modify these variables
+$driverDownloaded = "C:\support\PrinterDriver.zip"
+$extractPath = "C:\support\PrinterDriver"
+$portExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
+$printerExists = Get-Printer -Name $printerName -ErrorAction SilentlyContinue
+#####################
+# Waving magic wand #
+#####################
+if (-not $printerExists) {
+New-Item -ItemType Directory -Force -Path C:\support # Create local storage folder
+Invoke-WebRequest $driverDownloadPath -OutFile $driverDownloaded # Download HP driver
+Expand-Archive -Path $driverDownloaded -DestinationPath $extractPath -Force # Extract HP driver
+Get-ChildItem $extractPath -Recurse -Filter "*.inf" -Force | ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install } # Add to Windows Driver Store
+Add-PrinterDriver -Name $driverName # Add Driver
+# Add Printer Port
+if (-not $portExists) {
+  Add-PrinterPort -Name $portName -PrinterHostAddress $portAddress
+}
+# Install Printer
+if (-not $printerExists) {
+Add-Printer -Name $printerName -PortName $portName -DriverName $driverName
+}
+Set-PrintConfiguration -PrinterName $printerName -Color $true # Set Default to Color print
+(Get-WmiObject -ClassName Win32_Printer | Where-Object -Property Name -EQ $printerName).SetDefaultPrinter() # Set as default printer
+# Cleanup
+Remove-Item -Path $driverDownloaded -Force
+Remove-Item -Path $extractPath -Force -Recurse
+}
