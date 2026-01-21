67495d6e36f7011ba928a008dca5ca6d5766e25c
noterran <oleher90@gmail.com>
2026-01-21 14:59:55 +0100

    Updated EGS_ServerAppWinget.ps1 Now includes event logging.

--- Update-EGS_ServerAppWinget.ps1
+++ Update-EGS_ServerAppWinget.ps1
@@ -2,6 +2,7 @@ try {
     $wingetVersion = winget --version 2>&1
     Write-Host "WinGet is installed. Version: $wingetVersion"
 }
+
 catch {
     Write-Host "WinGet is not installed or not recognized."
     $progressPreference = 'silentlyContinue'
@@ -12,9 +13,28 @@ catch {
     Repair-WinGetPackageManager -AllUsers
     Write-Host "Done."
 }
-$apps = winget upgrade
-foreach ($app in $apps) {
-    if ($app -match "example_id") {
-        winget install --id=example_id --source=winget --silent --accept-package-agreements --accept-source-agreements --override "/S"
-    }
+
+$sourceName = "Update-EGS_ServerAppWinget"
+$logName = "Application"
+
+# Check if the source exists
+if (![System.Diagnostics.EventLog]::SourceExists($sourceName)) {
+    # If not, create it. Requires administrative privileges.
+    New-EventLog -LogName $logName -Source $sourceName
+    Write-Host "Created new event log source: $sourceName"
+} else {
+    Write-Host "Event log source already exists: $sourceName"
 }
+
+$appList = @("WinSCP.WinSCP")
+
+foreach ($app in $appList) {
+    try {
+        winget upgrade --id=$app --source=winget --silent --accept-package-agreements --accept-source-agreements --force
+        Write-EventLog -LogName $logName -Source $sourceName -EventId 3001 -EntryType Information -Message "Winget ran successfully for $app" -Category 1 -RawData 10,20
+        $result
+    }
+    catch {
+        Write-EventLog -LogName $logName -Source $sourceName -EventID 3002 -EntryType Information -Message "Winget failed to upgrade $app" -Category 1 -RawData 10,20
+    }      
+ }
