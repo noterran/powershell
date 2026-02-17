862483161a740e2357d31ed9674b7d4047b97da8
admOleHer <ole.anders.herland@marcello.no>
2026-02-17 11:50:12 +0100

    Revomed noisy write hosts

--- Get-ADGroup_Computer_Members.ps1
+++ Get-ADGroup_Computer_Members.ps1
@@ -4,7 +4,6 @@ Write-Host "Input the computer name and press enter"
 $ComputerName = Read-Host
 
 $ComputerDN = Get-ADComputer -Identity "$ComputerName" | Select-Object DistinguishedName
-write-host $ComputerDN
 
 $originalString = "$computerDN"
 $charsFromStart = 20 
@@ -13,10 +12,10 @@ $charsFromEnd = 1
 $newLength = $originalString.Length - $charsFromStart - $charsFromEnd
 
 $cleanComputerDN = $originalString.Substring($charsFromStart, $newLength)
-write-host $cleanComputerDN
 
 # Get all groups that the computer is a member of
 $allGroups = Get-ADPrincipalGroupMembership -Identity "$cleanComputerDN" |
     Select-Object Name
 
 $allGroups | Export-Csv -Path "C:\temp\$computerName-GroupMembership.csv" -NoTypeInformation
+write-host Result can be found in "C:\temp\$computerName-GroupMembership.csv"
