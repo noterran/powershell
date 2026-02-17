6c5ee3bb427cc53e175b528275081aadb7cd9155
admOleHer <ole.anders.herland@marcello.no>
2026-02-17 11:47:32 +0100

    Fixed bugs

--- Get-ADGroup_Computer_Members.ps1
+++ Get-ADGroup_Computer_Members.ps1
@@ -1,12 +1,22 @@
-import-mobule ActiveDirectory
-#Get the computer name from the user
+import-module ActiveDirectory
+# Get the computer name from the user
 Write-Host "Input the computer name and press enter"
-$ComputerName = Read-Host
+$ComputerName = "fst-app-001"
 
-$computerDN = Get-ADComputer -Filter { sAMAccountName -eq $env:COMPUTERNAME } | Select-Object DistinguishedName
+$ComputerDN = Get-ADComputer -Identity "$ComputerName" | Select-Object DistinguishedName
+write-host $ComputerDN
+
+$originalString = "$computerDN"
+$charsFromStart = 20 
+$charsFromEnd = 1
+
+$newLength = $originalString.Length - $charsFromStart - $charsFromEnd
+
+$cleanComputerDN = $originalString.Substring($charsFromStart, $newLength)
+write-host $cleanComputerDN
 
 # Get all groups that the computer is a member of
-$allGroups = Get-ADPrincipalGroupMembership -Identity $ComputerDN |
-    Select-Object Name, SamAccountName, DistinguishedName
+$allGroups = Get-ADPrincipalGroupMembership -Identity "$cleanComputerDN" |
+    Select-Object Name
 
-$allGroups | Export-Csv -Path "C:\Temp\$ComputerName-GroupMembership.csv" -NoTypeInformation
+$allGroups | Export-Csv -Path "C:\tmp\test-GroupMembership.csv" -NoTypeInformation
