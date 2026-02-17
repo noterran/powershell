3d507338e5d6f87f33a748ce0e81d3dd2a60cc9d
noterran <oleher90@gmail.com>
2026-02-17 10:55:19 +0100

    New script for getting AD Group Computer Members.

---
+++ Get-ADGroup_Computer_Members.ps1
@@ -0,0 +1,9 @@
+#Get the computer name from the user
+Write-Host "Input the computer name and press enter"
+$ComputerName = Read-Host
+
+# Get all groups that the computer is a member of
+$allGroups = Get-ADPrincipalGroupMembership -Identity $ComputerName |
+    Select-Object Name, SamAccountName, DistinguishedName
+
+$allGroups | Export-Csv -Path "C:\Temp\$ComputerName-GroupMembership.csv" -NoTypeInformation
