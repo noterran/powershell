4cd6cd54f21d52b42a34c5331df50b542241dbc7
admOleHer <ole.anders.herland@marcello.no>
2026-02-17 11:31:11 +0100

    Fetching DN

--- Get-ADGroup_Computer_Members.ps1
+++ Get-ADGroup_Computer_Members.ps1
@@ -3,32 +3,10 @@ import-mobule ActiveDirectory
 Write-Host "Input the computer name and press enter"
 $ComputerName = Read-Host
 
+$computerDN = Get-ADComputer -Filter { sAMAccountName -eq $env:COMPUTERNAME } | Select-Object DistinguishedName
+
 # Get all groups that the computer is a member of
-$allGroups = Get-ADPrincipalGroupMembership -Identity $ComputerName |
+$allGroups = Get-ADPrincipalGroupMembership -Identity $ComputerDN |
     Select-Object Name, SamAccountName, DistinguishedName
 
 $allGroups | Export-Csv -Path "C:\Temp\$ComputerName-GroupMembership.csv" -NoTypeInformation
-
-
-
-# Set the computer name (SAMAccountName)
-$ComputerName = "COMPUTER1$"
-
-# Create DirectorySearcher
-$Searcher = New-Object System.DirectoryServices.DirectorySearcher
-$Searcher.Filter = "(&(objectClass=computer)(sAMAccountName=$ComputerName))"
-$Searcher.PropertiesToLoad.Add("memberOf") | Out-Null
-
-# Run the query
-$Result = $Searcher.FindOne()
-
-if ($Result -ne $null) {
-    $Groups = $Result.Properties["memberOf"]
-
-    Write-Host "Groups for $ComputerName:`n"
-    foreach ($g in $Groups) {
-        Write-Host $g
-    }
-} else {
-    Write-Host "Computer not found in Active Directory."
-}
