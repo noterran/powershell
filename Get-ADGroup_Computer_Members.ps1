9d2c7cccf78cfa43c159e124be8d7dd049d2eca0
noterran <oleher90@gmail.com>
2026-02-17 11:24:02 +0100

    testing

--- Get-ADGroup_Computer_Members.ps1
+++ Get-ADGroup_Computer_Members.ps1
@@ -7,3 +7,27 @@ $allGroups = Get-ADPrincipalGroupMembership -Identity $ComputerName |
     Select-Object Name, SamAccountName, DistinguishedName
 
 $allGroups | Export-Csv -Path "C:\Temp\$ComputerName-GroupMembership.csv" -NoTypeInformation
+
+
+
+# Set the computer name (SAMAccountName)
+$ComputerName = "COMPUTER1$"
+
+# Create DirectorySearcher
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher
+$Searcher.Filter = "(&(objectClass=computer)(sAMAccountName=$ComputerName))"
+$Searcher.PropertiesToLoad.Add("memberOf") | Out-Null
+
+# Run the query
+$Result = $Searcher.FindOne()
+
+if ($Result -ne $null) {
+    $Groups = $Result.Properties["memberOf"]
+
+    Write-Host "Groups for $ComputerName:`n"
+    foreach ($g in $Groups) {
+        Write-Host $g
+    }
+} else {
+    Write-Host "Computer not found in Active Directory."
+}
